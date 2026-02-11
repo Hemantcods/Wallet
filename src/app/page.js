ab65fe8eaddfc7fab727e1d3f48217c15cbec8d5
Hemantcodes <hy564636@gmail.com>
2026-02-12 00:27:04 +0530

    made ui only

--- src/app/page.js
+++ src/app/page.js
@@ -1,65 +1,58 @@
+"use client";
 import Image from "next/image";
+import sol from "../assets/sol.png";
+import eth from "../assets/eth.png";
+import { useState } from "react";
+import Wallet from './../componets/Wallet';
 
 export default function Home() {
+  const [section, setSection] = useState("home");
+  const [wallet, setWallet] = useState(false)
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+    <>
+      {section === "home" && (
+        <div className="h-screen w-screen centre flex-col -mt-20">
+          <h3 className="text-4xl text-white font-orbitron p-4 pb-10">
+            Choose Your Wallet
+          </h3>
+          <div className="blockchain h-100  text-white gap-10 centre ">
+            <div
+              className="ethereum group h-100 w-100  text-white centre flex-col gap-12 rounded-3xl bg-black hover:cursor-pointer  "
+              onClick={() => {
+                setSection("eth");
+              }}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              <div className="centre h-60 w-60 transition-transform duration-300 ease-in-out group-hover:scale-105">
+                <Image src={eth} />
+              </div>
+              <h4 className="font-orbitron text-2xl">Ethereum</h4>
+            </div>
+            <div
+              className="solana group h-100 w-100 relative text-white rounded-3xl centre flex-col gap-12 bg-black hover:cursor-pointer"
+              onClick={() => {
+                setSection("sol");
+              }}
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              <div className="centre h-60 w-60 transition-transform duration-300 ease-in-out group-hover:scale-105">
+                <Image src={sol} />
+              </div>
+              <h4 className="font-orbitron text-2xl">Solana</h4>
+            </div>
+          </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+      )}
+      {section === "sol" && (
+        <div className=" w-full h-full  flex flex-col ">
+          <div className="top w-full h-50  mt-20 centre gap-10 px-50  flex-col flex">
+            <h1 className="text-white text-3xl font-orbitron ">Set Recovery Phase</h1>
+            <div className="w-full flex px-10 gap-10">
+              <textarea className="bg-white rounded-2xl h-10 w-full resize-none outline-none p-2"></textarea>
+              <button className="h-10 w-30 rounded-2xl  bg-red-500 text-white font-orbitron " onClick={()=>{setWallet(!wallet)}} >Generate</button>
+            </div>
+          </div>
+          {wallet && <Wallet/>}
         </div>
-      </main>
-    </div>
+      )}
+    </>
   );
 }
